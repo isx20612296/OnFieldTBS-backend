cbae9795344aed14bda2f2dcd609eadc9c29f121
iam49949708 <iam49949708@i29.informatica.escoladeltreball.org>
2022-04-25 18:27:45 +0200

    añadido el esqueleto del backend

---
+++ OnFieldTBS/src/main/java/org/proyectofinal/OnFieldTBS/OnFieldTbsApplication.java
@@ -0,0 +1,13 @@
+package org.proyectofinal.OnFieldTBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnFieldTbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnFieldTbsApplication.class, args);
+	}
+
+}
