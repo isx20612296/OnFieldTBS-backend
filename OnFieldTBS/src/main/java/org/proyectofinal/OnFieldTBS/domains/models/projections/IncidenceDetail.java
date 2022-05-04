4add7308e5c69d6fa22494e022103cb88427fb6c
andreupasalamar <andreupas23@gmail.com>
2022-05-04 18:27:44 +0200

    Modifico el orden de los campos de IncidenceDetail y creo directorio disenyo

--- OnFieldTBS/src/main/java/org/proyectofinal/OnFieldTBS/domains/models/projections/IncidenceDetail.java
+++ OnFieldTBS/src/main/java/org/proyectofinal/OnFieldTBS/domains/models/projections/IncidenceDetail.java
@@ -1,9 +1,12 @@
 package org.proyectofinal.OnFieldTBS.domains.models.projections;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
+@JsonPropertyOrder({"id", "title", "description","employee", "company","technician","state", "priority", "createdAt", "closedAt"})
 public interface IncidenceDetail {
      UUID getId();
      String getTitle();
